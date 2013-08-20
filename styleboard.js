94d3b3c2cae529a7c54f237ee50894dc76dba96a
kkubiak <kkubiak@apple.com>
2013-08-20 13:54:14 -0700

    full-screen app layout, with example written into an iframe

--- styleboard.js
+++ styleboard.js
@@ -1,13 +1,14 @@
 function StyleBoard( opts ) {
-    var env = {},
-        selectors = [],
+    var sb = this,
+        env = {},
+        cssUrl = opts.cssUrl || 'samples/home.css',
         $modules = $('#modules'),
         $selectors = $('#selectors'),
         dictionary = new Dictionary();
         analyzer = new Analyzer( dictionary ),
         parser = new Parser();
 
-    parser.load( opts.cssUrl, function (rules) {
+    parser.load( cssUrl, function (rules) {
         rules.forEach( showRule );
         analyzer.analyze( rules );
         dictionary.each( function (module) {
@@ -15,7 +16,15 @@ function StyleBoard( opts ) {
         });
     });
 
-    return this;
+    sb.loadExample = function( example ) {
+        var doc = $('#sandbox')[0].contentWindow.document;
+        doc.open();
+        doc.write('<html lang="en"><head><meta charset="utf-8"><link rel="stylesheet" type="text/css" href="' +
+                  cssUrl + '"></head><body>' + example + '</body></html>');
+        doc.close();
+    }
+
+    return sb;
 
     // this is just debugging stuff for now; might use it to show the CSS
     function showRule( rule ) {
@@ -35,6 +44,7 @@ function StyleBoard( opts ) {
             $li.mk( 'code', rule.toCSS(env) );
         }
     }
+
 }
 
 function Dictionary() {
